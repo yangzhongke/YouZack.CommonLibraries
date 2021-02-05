8beca4a80147c62658c6d5b2520ec39fb72e37cb
yzk <yzk365@qq.com>
2021-02-06 07:47:14 +0800

    Add BizException

---
+++ YouZack.Helpers/BizException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouZack.Helpers
+{
+    public class BizException:ApplicationException
+    {
+        public BizException() : base() { }
+        
+        public BizException(string? message) : base(message) { }
+
+        public BizException(string? message, Exception? innerException) : base(message,innerException) { }
+
+        public int Code { get; set; }
+        public string Status { get; set; }
+    }
+}
